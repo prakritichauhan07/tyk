82bd2ab8d15a33c285a01d90e09d695233c81fbb
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:12:04 +0530

    Update mw_basic_auth_test.go

--- gateway/mw_basic_auth_test.go
+++ gateway/mw_basic_auth_test.go
@@ -7,11 +7,11 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/TykTechnologies/tyk/storage"
+	"github.com/prakritichauhan07/tyk/storage"
 
-	"github.com/TykTechnologies/tyk/config"
-	"github.com/TykTechnologies/tyk/test"
-	"github.com/TykTechnologies/tyk/user"
+	"github.com/prakritichauhan07/tyk/config"
+	"github.com/prakritichauhan07/tyk/test"
+	"github.com/prakritichauhan07/tyk/user"
 )
 
 func genAuthHeader(username, password string) string {
