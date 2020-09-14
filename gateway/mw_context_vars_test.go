b310e9066436040e9a4a145da461277924dabd18
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:12:49 +0530

    Update mw_context_vars_test.go

--- gateway/mw_context_vars_test.go
+++ gateway/mw_context_vars_test.go
@@ -9,8 +9,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/TykTechnologies/tyk/apidef"
-	"github.com/TykTechnologies/tyk/test"
+	"github.com/prakritichauhan07/tyk/apidef"
+	"github.com/prakritichauhan07/tyk/test"
 )
 
 func testPrepareContextVarsMiddleware() {
