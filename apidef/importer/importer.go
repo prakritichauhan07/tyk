ea6990829bd9c8a27d026294a11339de6f6c9b5a
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 10:32:17 +0530

    Update importer.go

--- apidef/importer/importer.go
+++ apidef/importer/importer.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"io"
 
-	"github.com/TykTechnologies/tyk/apidef"
-	logger "github.com/TykTechnologies/tyk/log"
+	"github.com/prakritichauhan07/tyk/apidef"
+	logger "github.com/prakritichauhan07/tyk/log"
 )
 
 var log = logger.Get()
