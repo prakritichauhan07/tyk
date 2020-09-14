6240be92df42bf776f88e0e0661535e80135ff6b
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:19:48 +0530

    Update mw_key_expired_check.go

--- gateway/mw_key_expired_check.go
+++ gateway/mw_key_expired_check.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/TykTechnologies/tyk/request"
+	"github.com/prakritichauhan07/tyk/request"
 )
 
 // KeyExpired middleware will check if the requesting key is expired or not. It makes use of the authManager to do so.
