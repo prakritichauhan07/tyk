54ac9cacdd3c50d0c449fae2d2137c2c6f59598f
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:41:39 +0530

    Update mw_version_check.go

--- gateway/mw_version_check.go
+++ gateway/mw_version_check.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/TykTechnologies/tyk/apidef"
-	"github.com/TykTechnologies/tyk/request"
+	"github.com/prakritichauhan07/tyk/apidef"
+	"github.com/prakritichauhan07/tyk/request"
 )
 
 // VersionCheck will check whether the version of the requested API the request is accessing has any restrictions on URL endpoints
