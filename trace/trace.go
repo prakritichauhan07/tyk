f9c22ed04826bd9db86c8a0b36089b0125becdcf
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 12:04:30 +0530

    Update trace.go

--- trace/trace.go
+++ trace/trace.go
@@ -3,8 +3,8 @@ package trace
 import (
 	"io"
 
-	"github.com/TykTechnologies/tyk/trace/jaeger"
-	"github.com/TykTechnologies/tyk/trace/openzipkin"
+	"github.com/prakritichauhan07/tyk/trace/jaeger"
+	"github.com/prakritichauhan07/tyk/trace/openzipkin"
 	opentracing "github.com/opentracing/opentracing-go"
 )
 
