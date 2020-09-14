eed2ead337ad7a72ea533e4fb187f14a14bfe51f
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:34:41 +0530

    Update mw_transform_jq_test.go

--- gateway/mw_transform_jq_test.go
+++ gateway/mw_transform_jq_test.go
@@ -5,8 +5,8 @@ package gateway
 import (
 	"testing"
 
-	"github.com/TykTechnologies/tyk/apidef"
-	"github.com/TykTechnologies/tyk/test"
+	"github.com/prakritichauhan07/tyk/apidef"
+	"github.com/prakritichauhan07/tyk/test"
 )
 
 func testPrepareJQMiddleware() {
