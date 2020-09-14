ea3e60fcc035a2f48796ca26da2e615d77ea917e
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:08:42 +0530

    Update monitor.go

--- gateway/monitor.go
+++ gateway/monitor.go
@@ -3,8 +3,8 @@ package gateway
 import (
 	"time"
 
-	"github.com/TykTechnologies/tyk/config"
-	"github.com/TykTechnologies/tyk/user"
+	"github.com/prakritichauhan07/tyk/config"
+	"github.com/prakritichauhan07/tyk/user"
 )
 
 type Monitor struct{}
