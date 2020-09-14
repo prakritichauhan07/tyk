c0d7a0a65353dadf0d9d0f679535b310fb39ad19
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:22:44 +0530

    Update mw_oauth2_key_exists.go

--- gateway/mw_oauth2_key_exists.go
+++ gateway/mw_oauth2_key_exists.go
@@ -5,9 +5,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/TykTechnologies/tyk/config"
+	"github.com/prakritichauhan07/tyk/config"
 
-	"github.com/TykTechnologies/tyk/apidef"
+	"github.com/prakritichauhan07/tyk/apidef"
 )
 
 const (
