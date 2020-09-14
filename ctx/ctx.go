170c7b6facc44237dafe1a3bec6114e374eb452f
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 10:39:52 +0530

    Update ctx.go

--- ctx/ctx.go
+++ ctx/ctx.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/TykTechnologies/tyk/apidef"
+	"github.com/prakritichauhan07/tyk/apidef"
 
-	"github.com/TykTechnologies/tyk/storage"
-	"github.com/TykTechnologies/tyk/user"
+	"github.com/prakritichauhan07/tyk/storage"
+	"github.com/prakritichauhan07/tyk/user"
 )
 
 type Key uint
