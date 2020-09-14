c69c64c501819c7dbb2e75ec66f336e5ac92a4fa
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:45:39 +0530

    Update policy.go

--- gateway/policy.go
+++ gateway/policy.go
@@ -9,11 +9,11 @@ import (
 
 	"github.com/jensneuse/graphql-go-tools/pkg/graphql"
 
-	"github.com/TykTechnologies/tyk/rpc"
+	"github.com/prakritichauhan07/tyk/rpc"
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/TykTechnologies/tyk/user"
+	"github.com/prakritichauhan07/tyk/user"
 )
 
 type DBAccessDefinition struct {
