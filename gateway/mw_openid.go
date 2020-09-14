c930e0b4c8282da3b033dc3759b7a15d77ad2e36
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:23:14 +0530

    Update mw_openid.go

--- gateway/mw_openid.go
+++ gateway/mw_openid.go
@@ -12,8 +12,8 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/TykTechnologies/openid2go/openid"
-	"github.com/TykTechnologies/tyk/apidef"
-	"github.com/TykTechnologies/tyk/user"
+	"github.com/prakritichauhan07/tyk/apidef"
+	"github.com/prakritichauhan07/tyk/user"
 )
 
 const OIDPREFIX = "openid"
