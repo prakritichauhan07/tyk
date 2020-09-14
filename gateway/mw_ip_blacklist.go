6bb19de847890a5e3e7b05433ec67e373d2ec50c
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:16:43 +0530

    Update mw_ip_blacklist.go

--- gateway/mw_ip_blacklist.go
+++ gateway/mw_ip_blacklist.go
@@ -5,7 +5,7 @@ import (
 	"net"
 	"net/http"
 
-	"github.com/TykTechnologies/tyk/request"
+	"github.com/prakritichauhan07/tyk/request"
 )
 
 // IPBlackListMiddleware lets you define a list of IPs to block from upstream
