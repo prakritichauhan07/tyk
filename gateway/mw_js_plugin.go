a680eb113f3bbfe92b412f27a54e93a3578334ce
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:17:35 +0530

    Update mw_js_plugin.go

--- gateway/mw_js_plugin.go
+++ gateway/mw_js_plugin.go
@@ -18,8 +18,8 @@ import (
 	"github.com/robertkrimen/otto"
 	_ "github.com/robertkrimen/otto/underscore"
 
-	"github.com/TykTechnologies/tyk/config"
-	"github.com/TykTechnologies/tyk/user"
+	"github.com/prakritichauhan07/tyk/config"
+	"github.com/prakritichauhan07/tyk/user"
 
 	"github.com/sirupsen/logrus"
 )
