d127d0633827716b5352b20f801461ac3104abc1
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-09-14 11:40:22 +0530

    Update mw_validate_json.go

--- gateway/mw_validate_json.go
+++ gateway/mw_validate_json.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 
 	"github.com/TykTechnologies/gojsonschema"
-	"github.com/TykTechnologies/tyk/apidef"
+	"github.com/prakritichauhan07/tyk/apidef"
 )
 
 type ValidateJSON struct {
